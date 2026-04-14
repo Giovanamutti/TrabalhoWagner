45390acd7b11d9ef5154d915735de458553b857c
Giovanamutti <Giovana.mutti@escola.pr.gov.br>
2026-04-14 19:05:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -62,3 +62,20 @@ if (dados) {
     // Caso id for inexistente
     console.error("Produto não encontrado!");
 }
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    
+    // Animação básica das barrinhas do menu (opcional)
+    mobileMenu.classList.toggle('is-active');
+});
+
+// Fechar o menu ao clicar em qualquer link (bom para mobile)
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
